2c0f91e10e3d5bae4f96d452f6b188c8eaa42dd8
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2025-01-30 14:28:41 +0100

    fix wsgi import

--- stage_0/src/wsgi.py
+++ stage_0/src/wsgi.py
@@ -5,9 +5,8 @@ web server gateway
 Application entry point in production
 """
 
-from server import app
-
 from . import config
+from .server import server as app
 
 if __name__ == "__main__":
     if config.env == 'prod':
